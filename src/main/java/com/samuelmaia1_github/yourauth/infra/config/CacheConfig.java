b2f5b5446d765bf627e29e93f01cce970d9341d8
samuelmaia1 <smaia190500@gmail.com>
2026-09-08 17:08:32 -0300

    (config): update git ignore and config cache

---
+++ src/main/java/com/samuelmaia1_github/yourauth/infra/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.samuelmaia1_github.yourauth.infra.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+@EnableCaching
+public class CacheConfig {
+}
